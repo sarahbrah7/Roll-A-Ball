181d7c1fbb03803fc0dd71cd151981238b7b505f
Sarah Brahimi <71637499+sarahbrah7@users.noreply.github.com>
2022-10-13 10:03:41 +0100

    count and collision made

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    private void Start()
+    {
+        count = 0;
+    }
 
     //captures the input
     //saves the value recieved as input (input is WASD or arrow keys)
@@ -20,5 +26,16 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    //called when an object collider is triggered and collides with the collider of the game object
+    //which has the script as a component
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+        }
     }
 }
